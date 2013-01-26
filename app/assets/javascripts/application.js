d792aaf5ad6b4c61c8e5d27aeefa7e05797cbafd
hrigu <chmuehlethaler@gmail.com>
2013-01-26 09:54:39 +0100

    sonst was

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,19 +8,19 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require google-code-prettify
-//
+//= require_tree .
 
-$(function() {
-  prettyPrint();
 
-  $('a[data-code]').click(function(e) {
-    e.preventDefault();
-    $('pre').slideToggle();
-  });
+$(function () {
+    prettyPrint();
 
-    $('#toggle-form').on('click', function(e) {
-      e.preventDefault();
-      $('.simple_form').toggleClass('form-vertical').toggleClass('form-horizontal');
+    $('a[data-code]').click(function (e) {
+        e.preventDefault();
+        $('pre').slideToggle();
+    });
+
+    $('#toggle-form').on('click', function (e) {
+        e.preventDefault();
+        $('.simple_form').toggleClass('form-vertical').toggleClass('form-horizontal');
     })
 });
-
